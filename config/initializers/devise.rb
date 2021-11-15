a042877440ac0a7ce06ff60da6d6c62929a7c6ad
Nicoleta Faina <nicoletafaina.contact@gmail.com>
2021-11-15 12:25:15 +0000

    Add devise user model and migration

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-# Devise mailer initializer
-config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-
 # Assuming you have not yet modified this file, each configuration option below
 # is set to its default value. Note that some are commented out while others
 # are not: uncommented lines are intended to protect your configuration from
