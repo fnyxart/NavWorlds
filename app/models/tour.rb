465acceb50d92a78c507190574eed0da73498564
Nicoleta Faina <nicoletafaina.contact@gmail.com>
2021-11-18 13:30:30 +0000

    Add attached image to tour model

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,6 +1,7 @@
 class Tour < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+  has_one_attached :photo
 
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
