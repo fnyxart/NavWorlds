fa4aa3156f4486ab1a87970b76dae4d526152a75
Louisa Noffke <louno@lounovo.com>
2021-11-18 12:32:44 +0000

    started on search feature: installed pg-search gem

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -5,4 +5,11 @@ class Tour < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :description, presence: true
   validates :price, presence: true, numericality: true
+
+  include PGSearch::Model
+  pg_search_scope :global_search,
+    against: [ :name, :description ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
